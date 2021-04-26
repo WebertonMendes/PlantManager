c5330bb4b3dfe622c9ddb83fa94584a33a010a6a
WebertonMendes <weberton.mendes@hotmail.com>
2021-04-26 14:26:25 -0300

    PlantManager

--- App.tsx
+++ App.tsx
@@ -1,7 +1,11 @@
-import React from "react";
-import AppLoading from "expo-app-loading"
+import React, { useEffect } from "react";
+import AppLoading from "expo-app-loading";
+import * as Notifications from 'expo-notifications';
 
 import Routes from "./src/routes";
+
+import { PlantProps } from "./src/libs/storage";
+
 import {
   useFonts,
   Jost_400Regular,
@@ -13,6 +17,30 @@ export default function App() {
     Jost_400Regular,
     Jost_600SemiBold
   });
+
+  // useEffect(() => {
+    // Listen Notifications
+    // const subscription = Notifications.addNotificationReceivedListener(
+    //   async notification => {
+    //     const data = notification.request.content.data.plant as PlantProps
+    //     console.log(data);
+    //   }
+    // );
+
+    // return () => subscription.remove();
+
+    // async function notifications() {
+      // Cancel all Scheduled Notifications
+      // await Notifications.cancelAllScheduledNotificationsAsync();
+
+      // Get all Scheduled Notifications
+      // const data = await Notifications.getAllScheduledNotificationsAsync();
+      // console.log("### NOTIFICAÇÕES AGENDADAS ###");
+      // console.log(data);
+    // }
+
+    // notifications();
+  // },[])
 
   if(!fontsLoaded)
     return <AppLoading />
